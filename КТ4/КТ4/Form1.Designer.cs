844d100c96e656f4a58e937634238fe422189870
Nikita Bestfator <nbestfator@gmail.com>
2025-12-01 22:08:14 +0300

    Доделал КТ4

--- КТ4/КТ4/Form1.Designer.cs
+++ КТ4/КТ4/Form1.Designer.cs
@@ -24,16 +24,48 @@ partial class Form1
     #region Windows Form Designer generated code
 
     /// <summary>
-    ///  Required method for Designer support - do not modify
-    ///  the contents of this method with the code editor.
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
     /// </summary>
     private void InitializeComponent()
     {
-        this.components = new System.ComponentModel.Container();
-        this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-        this.ClientSize = new System.Drawing.Size(800, 450);
-        this.Text = "Form1";
+        lbl = new System.Windows.Forms.Label();
+        btn = new System.Windows.Forms.Button();
+        SuspendLayout();
+        // 
+        // lbl
+        // 
+        lbl.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) | System.Windows.Forms.AnchorStyles.Left) | System.Windows.Forms.AnchorStyles.Right));
+        lbl.Location = new System.Drawing.Point(12, 9);
+        lbl.Name = "lbl";
+        lbl.Size = new System.Drawing.Size(683, 442);
+        lbl.TabIndex = 0;
+        lbl.Text = "Авиабилеты";
+        // 
+        // btn
+        // 
+        btn.Anchor = ((System.Windows.Forms.AnchorStyles)(System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right));
+        btn.Location = new System.Drawing.Point(498, 454);
+        btn.Name = "btn";
+        btn.Size = new System.Drawing.Size(197, 56);
+        btn.TabIndex = 1;
+        btn.Text = "Прочитать содержимое файла";
+        btn.UseVisualStyleBackColor = true;
+        btn.Click += btn_Click;
+        // 
+        // Form1
+        // 
+        AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+        AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+        ClientSize = new System.Drawing.Size(707, 522);
+        Controls.Add(btn);
+        Controls.Add(lbl);
+        Text = "КТ4: Загрузка файлов в форму";
+        ResumeLayout(false);
     }
+
+    private System.Windows.Forms.Label lbl;
+    private System.Windows.Forms.Button btn;
 
     #endregion
 }
--- КТ4/КТ4/Form1.cs
+++ КТ4/КТ4/Form1.cs
@@ -4,26 +4,17 @@ public partial class Form1 : Form
 {
     public Form1()
     {
-        string filePath = "tickets.txt";
-
-        Button button = new Button()
-        {
-            Text = "Прочитать содержимое файла",
-            Location = new Point(10, 10),
-            AutoSize = true
-        };
-        
-        
-        Label label = new Label()
-        {
-            Text = "Авиабилеты",
-            Location = new Point(10, 10),
-            AutoSize = true
-        };
-        button.Click += (s, e) =>
-        {
-            label.Text = File.ReadAllText(filePath);
-        };
         InitializeComponent();
+    }
+
+    private void btn_Click(object sender, EventArgs e)
+    {
+        using var ofd = new OpenFileDialog();
+        var r = ofd.ShowDialog();
+        if (r == DialogResult.OK)
+        {
+            var path = ofd.FileName;
+            lbl.Text = File.ReadAllText(path);
+        }
     }
 }
